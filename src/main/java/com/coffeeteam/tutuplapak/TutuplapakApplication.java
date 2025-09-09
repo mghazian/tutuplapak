c1fba0fe3ca1df772ea2a6a5ffd5fc8cc12a433c
Muhammad Ghazian <mghazian@github.com>
2025-09-09 14:45:16 +0700

    Initial commit

---
+++ src/main/java/com/coffeeteam/tutuplapak/TutuplapakApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeteam.tutuplapak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutuplapakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutuplapakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeteam/tutuplapak/TutuplapakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeteam.tutuplapak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutuplapakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
