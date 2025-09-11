8919993a8d865ad4de0a91b5976d2b2d98048e32
Muhammad Ghazian <mghazian@github.com>
2025-09-11 17:50:47 +0700

    feat: Save uploaded file to database

--- src/main/java/com/coffeeteam/tutuplapak/file/service/FileUploadServiceImpl.java
+++ src/main/java/com/coffeeteam/tutuplapak/file/service/FileUploadServiceImpl.java
@@ -3,6 +3,8 @@ package com.coffeeteam.tutuplapak.file.service;
 import com.coffeeteam.tutuplapak.file.dto.FileUploadRequestDto;
 import com.coffeeteam.tutuplapak.file.dto.FileUploadResponseDto;
 import com.coffeeteam.tutuplapak.file.dto.UploadResultDto;
+import com.coffeeteam.tutuplapak.file.model.Image;
+import com.coffeeteam.tutuplapak.file.repository.ImageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,19 +12,36 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadServiceImpl implements FileUploadService {
 
     private final ObjectStorageService objectStorageService;
+    private final ImageRepository imageRepository;
 
-    public FileUploadServiceImpl(ObjectStorageService objectStorageService) {
+    public FileUploadServiceImpl(
+            ObjectStorageService objectStorageService,
+            ImageRepository imageRepository
+    ) {
         this.objectStorageService = objectStorageService;
+        this.imageRepository = imageRepository;
     }
 
     @Override
     public FileUploadResponseDto upload(MultipartFile file) {
-        UploadResultDto result = objectStorageService.upload(file);
+        try {
+            UploadResultDto result = objectStorageService.upload(file);
 
-        return FileUploadResponseDto.builder()
-                .fileId("1")
-                .fileUri(result.getPresignedUrl())
-                .fileThumbnailUri(result.getPresignedUrl())
-                .build();
+            Image newImage = Image.builder()
+                    .ownerId(1L)
+                    .uri(result.getObjectName())
+                    .thumbnailUri(result.getObjectName())
+                    .build();
+
+            imageRepository.save(newImage);
+
+            return FileUploadResponseDto.builder()
+                    .fileId("1")
+                    .fileUri(newImage.getUri())
+                    .fileThumbnailUri(newImage.getThumbnailUri())
+                    .build();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
