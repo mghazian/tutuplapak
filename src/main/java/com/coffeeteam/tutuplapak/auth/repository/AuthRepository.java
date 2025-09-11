b8859212dd7bac3966a5e66a9cca1fb9d2e98797
Cesa <cesa263@gmail.com>
2025-09-11 21:30:42 +0700

    fix: removing unnecessary queries

--- src/main/java/com/coffeeteam/tutuplapak/auth/repository/AuthRepository.java
+++ src/main/java/com/coffeeteam/tutuplapak/auth/repository/AuthRepository.java
@@ -3,7 +3,6 @@ package com.coffeeteam.tutuplapak.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,42 +10,9 @@ import com.coffeeteam.tutuplapak.core.entity.User;
 
 @Repository
 public interface AuthRepository extends JpaRepository<User, Long>{
-
-    @Query("""
-        SELECT 
-            u
-        FROM 
-            User u
-        WHERE
-            u.email = :email        
-    """
-    )
     Optional<User> findByEmail(@Param("email") String email);
-
-    @Query("""
-        SELECT 
-            u
-        FROM 
-            User u
-        WHERE
-            u.phone = :phone        
-    """
-    )
     Optional<User> findByPhone(@Param("phone") String phone);
-
-    @Query("""
-        SELECT 
-            u
-        FROM 
-            User u
-        WHERE
-            u.phone = :phone
-            AND u.email = :email        
-    """
-    )
     Optional<User> findByPhoneAndEmail(@Param("phone") String phone, @Param("email") String email);
-
     boolean existsByEmail(String email);
-
     boolean existsByPhone(String email);
 }
