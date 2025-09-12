a46b5cbd33caaea5bb287a519d20bf4e891a2c6f
Muhammad Ghazian <mghazian@github.com>
2025-09-12 19:40:42 +0700

    fix: Cannot access resource even with valid JWT

---
+++ src/main/java/com/coffeeteam/tutuplapak/auth/security/PasswordStrategyConfig.java
@@ -0,0 +1,17 @@
+package com.coffeeteam.tutuplapak.auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordStrategyConfig {
+    public PasswordStrategyConfig() {
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
