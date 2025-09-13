311567e9bdac144dd714f85adf236ab6d8ef8d33
Cesa <cesa263@gmail.com>
2025-09-13 14:35:57 +0700

    feat: added one to one into User

--- src/main/java/com/coffeeteam/tutuplapak/core/entity/User.java
+++ src/main/java/com/coffeeteam/tutuplapak/core/entity/User.java
@@ -2,11 +2,15 @@ package com.coffeeteam.tutuplapak.core.entity;
 
 import java.time.OffsetDateTime;
 
+import com.coffeeteam.tutuplapak.file.model.Image;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -35,6 +39,9 @@ public class User {
     private String bankAccountNumber = "";
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
 
     @PrePersist
     protected void onCreate() {
