9a198de754e067e97dadf14886760d1223bc3d94
Muhammad Ghazian <mghazian@github.com>
2025-09-13 13:33:45 +0700

    feat: Use actual owner id for uploaded file

--- src/main/java/com/coffeeteam/tutuplapak/file/service/FileUploadService.java
+++ src/main/java/com/coffeeteam/tutuplapak/file/service/FileUploadService.java
@@ -1,9 +1,10 @@
 package com.coffeeteam.tutuplapak.file.service;
 
+import com.coffeeteam.tutuplapak.auth.security.CustomUserDetails;
 import com.coffeeteam.tutuplapak.file.dto.FileUploadRequestDto;
 import com.coffeeteam.tutuplapak.file.dto.FileUploadResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileUploadService {
-    FileUploadResponseDto upload (MultipartFile file);
+    FileUploadResponseDto upload(MultipartFile file, CustomUserDetails userDetails);
 }
